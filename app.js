647572594269c9b6208ba83171314ab9e6481f40
Roman <roma.skorobogatko@gmail.com>
2023-04-27 11:53:48 +0300

    /* = Ciao mondo!

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-var express = require('express');
+import express from "express"
+const PORT = 3000
+const app = express()
 
-var app = express();
+app.get('/*', (req, res) => {
+    res.status(200).json("Ciao mondo!")
+})
+
+app.listen(PORT, () => console.log("SERVER STARTED ON PORT " + PORT))
